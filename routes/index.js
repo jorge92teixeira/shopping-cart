ac8486670257b3b927880d03c337d65a486c6243
Jorge Teixeira <jorgemrt@protonmail.com>
2020-01-15 18:50:44 +0000

    shop now displays products in the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const products = await Product.find();
+  res.render('shop/index', { title: 'Shopping Cart', products });
 });
 
 module.exports = router;
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -28,31 +28,31 @@ const products = [
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg',
     title: 'The Witcher 3: Wild Hunt',
     description: 'The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt and based on The Witcher series of fantasy novels by Andrzej Sapkowski. It is the sequel to the 2011 game The Witcher 2: Assassins of Kings, played in an open world with a third-person perspective.',
-    price: 10,
+    price: 30,
   }),
   new Product({
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/8/8f/Metal_Gear_Solid_V_The_Phantom_Pain_cover.png',
     title: 'Metal Gear Solid V: The Phantom Pain',
     description: 'Metal Gear Solid V: The Phantom Pain is a stealth game developed by Kojima Productions and published by Konami. It was released worldwide for Microsoft Windows, PlayStation 3, PlayStation 4, Xbox 360 and Xbox One on September 1, 2015.',
-    price: 10,
+    price: 25,
   }),
   new Product({
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/c/c8/Shovel_knight_cover.jpg',
     title: 'Shovel Knight',
     description: 'Shovel Knight is a 2D side-scrolling platform game developed and published by Yacht Club Games. Following a successful Kickstarter crowdfunding campaign, the game was released for Microsoft Windows, Nintendo 3DS and Wii U in June 2014.',
-    price: 10,
+    price: 15,
   }),
   new Product({
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/b/b2/Ori_and_the_Blind_Forest_Logo.jpg',
     title: 'Ori and the Blind Forest',
     description: 'Ori and the Blind Forest is a platform-adventure Metroidvania video game developed by Moon Studios and published by Microsoft Studios. The game was released for Xbox One and Microsoft Windows on March 11, 2015 and was released for Nintendo Switch on September 27, 2019.',
-    price: 10,
+    price: 15,
   }),
   new Product({
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/2/28/Doom_Cover.jpg',
     title: 'Doom',
     description: 'Doom is a first-person shooter video game developed by id Software and published by Bethesda Softworks. It was released worldwide on Microsoft Windows, PlayStation 4, and Xbox One in May 2016 and is powered by id Tech 6. A port for Nintendo Switch was co-developed with Panic Button and released in November 2017. A reboot of the Doom franchise, it is the fourth title in the main series and the first major installment since Doom 3 in 2004.',
-    price: 10,
+    price: 20,
   }),
 ];
 
